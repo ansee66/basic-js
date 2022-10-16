58eb6b88703d93a7c5c0fe7ecd39daff23d8b91c
Natalya Solovyova <ns-66@yandex.ru>
2022-10-16 18:44:31 +0500

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direction) {
+    this.direction = direction;
+    this.letters = ['A', "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  generateKey(words, key) {
+    if (words.length !== key.length) {
+      key = key.split("");
+      let temp = key.length;   
+      for (let i = 0; i < (words.length - temp); i++) {
+        key.push(key[i]);
+      }
+      key = key.join("")
+    }
+    return key;
+  }
+  encrypt(words, key) {
+    if (typeof words === 'undefined') throw new Error("Incorrect arguments!");
+    if (typeof key === 'undefined') throw new Error("Incorrect arguments!");
+    key = this.generateKey(words, key).toUpperCase();
+    let result = [];
+    let ind = 0;
+    words.toUpperCase().split("").map((letter) => {
+      if (this.letters.indexOf(letter) !== -1) {
+        let index = (this.letters.indexOf(letter) + this.letters.indexOf(key[ind])) % 26;
+        result.push(this.letters[index]);
+        ind++;
+      } else {
+        result.push(letter);
+      }
+    })
+    if (this.direction === false) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    }
+  }
+  decrypt(words, key) {
+    if (typeof words === 'undefined') throw new Error("Incorrect arguments!");
+    if (typeof key === 'undefined') throw new Error("Incorrect arguments!");
+    key = this.generateKey(words, key).toUpperCase();
+    let result = [];
+    let ind = 0;
+    words.toUpperCase().split("").map((letter) => {
+      if (this.letters.indexOf(letter) !== -1) {
+        let index = (this.letters.indexOf(letter) - this.letters.indexOf(key[ind])) % 26;
+        if (index < 0) {
+          index = 26 + index;
+        }
+        result.push(this.letters[index]);
+        ind++;
+      } else {
+        result.push(letter);
+      }
+    })
+    if (this.direction === false) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    }
   }
 }
 
